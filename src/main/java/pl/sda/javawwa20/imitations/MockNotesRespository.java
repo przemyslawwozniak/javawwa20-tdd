49772e600b24049d4d65043e25bf63ece6e3ae7a
Przemysław Woźniak <inz.przemyslaw.wozniak@gmail.com>
2019-08-10 12:16:05 +0200

    Imitations (3)

--- src/main/java/pl/sda/javawwa20/imitations/MockNotesRespository.java
+++ src/main/java/pl/sda/javawwa20/imitations/MockNotesRespository.java
@@ -1,21 +1,35 @@
 package pl.sda.javawwa20.imitations;
 
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class MockNotesRespository implements NotesRepository {
 
+    private Map<String, List<Note>> nameToNotesMap = new HashMap<>();
+
     @Override
     public void save(Note note) {
-        
+        //1. uczen juz istnieje
+        if(nameToNotesMap.containsKey(note.getFullName())) {
+            List<Note> memberNotes = nameToNotesMap.get(note.getFullName());
+            memberNotes.add(note);
+            nameToNotesMap.put(note.getFullName(), memberNotes);
+        }
+        //2. zupelnie nowy uczen
+        else {
+            nameToNotesMap.put(note.getFullName(), Arrays.asList(note));
+        }
     }
 
     @Override
     public List<Note> getAllNotesOf(String fullName) {
-        return null;
+        return nameToNotesMap.get(fullName);
     }
 
     @Override
     public void removeAll() {
-
+        nameToNotesMap.clear();
     }
 }
---
+++ src/test/java/pl/sda/javawwa20/imitations/DefaultNotesServiceTest.java
@@ -0,0 +1,39 @@
+package pl.sda.javawwa20.imitations;
+
+import org.junit.Test;
+
+public class DefaultNotesServiceTest {
+
+    private NotesService notesService = DefaultNotesService.createWith(new MockNotesRespository());
+
+    @Test
+    public void add() {
+
+    }
+
+    @Test
+    public void add_null_note() {
+
+    }
+
+    @Test
+    public void average_of_null() {
+
+    }
+
+    @Test
+    public void average_of_non_existent() {
+
+    }
+
+    @Test
+    public void average_of_existing() {
+
+    }
+
+    @Test
+    public void clear() {
+
+    }
+
+}
