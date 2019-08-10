13c2265b106fcba8d522e194e1b7f1bb226ca00d
Przemysław Woźniak <inz.przemyslaw.wozniak@gmail.com>
2019-08-10 12:53:00 +0200

    Imitations (comments)

--- src/test/java/pl/sda/javawwa20/imitations/DefaultNotesServiceTest.java
+++ src/test/java/pl/sda/javawwa20/imitations/DefaultNotesServiceTest.java
@@ -8,32 +8,35 @@ public class DefaultNotesServiceTest {
 
     @Test
     public void add() {
-
+        //dodac poprawna ocene i sprawdzic czy zostala dodana
     }
 
     @Test
     public void add_null_note() {
-
+        //sprobuj jako parametr przekazac null
     }
 
     @Test
     public void average_of_null() {
-
+        //jako parametr null
     }
 
     @Test
     public void average_of_non_existent() {
-
+        //probujecie policzyc srednia ocen dla nieistniejacego ucznia
     }
 
     @Test
     public void average_of_existing() {
-
+        //sprawdzacie czy prawidlowo liczy srednia dla istniejacego ucznia
     }
 
     @Test
     public void clear() {
-
+        //dodaj 2 uczniow i ich oceny
+        //odczytaj ze sa
+        //zrob clear
+        //sprobuj odczytac ponownie
     }
 
 }
